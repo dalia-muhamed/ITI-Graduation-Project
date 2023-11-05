55e71c319d732f662781e7b65af918dd4232a0bf
Kerollos Fessal <kerofessal77@gmail.com>
2023-11-05 04:14:35 +0200

    successfully

--- src/pages/singlePage/SinglePage.jsx
+++ src/pages/singlePage/SinglePage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { axiosInstance } from '../../axios';
 import './SinglePage.css';
 import pen from './singlePageIcons/pen.png';
