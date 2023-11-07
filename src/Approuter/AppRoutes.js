9e044b8d6e6c3c248e8206d0daeeb30ec248e021
mina karam <minakaramkhalil.28@gmail.com>
2023-11-07 09:22:06 +0200

    push

--- src/Approuter/AppRoutes.js
+++ src/Approuter/AppRoutes.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from '../pages/home/Home';
-import Hotels from '../pages/hotels/Hotels';
-import ThingsToDo from '../pages/thingsToDo/ThingsToDo';
-import Resturants from '../pages/restaurants/Resturants';
-import SinglePage from '../pages/singlePage/SinglePage';
-import HotelReservation from '../pages/reservation/hotel-reservation/hotel-reservation';
-import NotFound from '../NotFound/Notfound';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/home/Home";
+import Hotels from "../pages/hotels/Hotels";
+import ThingsToDo from "../pages/thingsToDo/ThingsToDo";
+import Resturants from "../pages/restaurants/Resturants";
+import SinglePage from "../pages/singlePage/SinglePage";
+import HotelReservation from "../pages/reservation/hotel-reservation/hotel-reservation";
+import NotFound from "../NotFound/Notfound";
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cities/hotels" element={<Hotels />} />
-        <Route path="/cities/hotels/reservation" element={<HotelReservation />} />
+        <Route
+          path="/cities/hotels/reservation"
+          element={<HotelReservation />}
+        />
         <Route path="/cities/restaurants" element={<Resturants />} />
         <Route path="/cities/thingsToDo" element={<ThingsToDo />} />
         <Route
