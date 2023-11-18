6ccf876faae8ba5263eb431231c78b5d9597fee1
dalia.muhammed <dalia.muhammed1525@gmail.com>
2023-11-18 19:34:06 +0200

    fixing navbar

--- src/components/GoogleLogin/GoogleLogin.js
+++ src/components/GoogleLogin/GoogleLogin.js
@@ -43,23 +43,25 @@ import { useState } from 'react';
 import { jwtDecode } from 'jwt-decode';
 
 const GoogleLogin = () => {
-  const [imageSrc, setImageSrc] = useState('https://www.mgp.net.au/wp-content/uploads/2023/05/150-1503945_transparent-user-png-default-user-image-png-png.png');
+  const [imageSrc, setImageSrc] = useState(
+    'https://www.mgp.net.au/wp-content/uploads/2023/05/150-1503945_transparent-user-png-default-user-image-png-png.png'
+  );
 
   useGoogleOneTapLogin({
-    onSuccess: (credentialResponse) => {
-      const credentialResponseDecoded = jwtDecode(credentialResponse.credential);
+    onSuccess: credentialResponse => {
+      const credentialResponseDecoded = jwtDecode(
+        credentialResponse.credential
+      );
       setImageSrc(credentialResponseDecoded.picture);
       localStorage.setItem('hasLogged', 'true');
       localStorage.setItem('userImage', credentialResponseDecoded.picture);
     },
-    onError: (error) => {
+    onError: error => {
       console.error('Error during login:', error);
     },
   });
 
-  return <img  src={imageSrc} />;
+  return <img src={imageSrc} />;
 };
 
 export default GoogleLogin;
-
-
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,15 @@
-import logo from "./logo.jpg";
-import "./navbar.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { useState, useEffect } from "react";
-import GoogleLogin from "../GoogleLogin/GoogleLogin";
-import { jwtDecode } from "jwt-decode";
-import { Link } from "react-router-dom";
-import { faBookmark } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import logo from './logo.jpg';
+import './navbar.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { useState, useEffect } from 'react';
+import GoogleLogin from '../GoogleLogin/GoogleLogin';
+import { jwtDecode } from 'jwt-decode';
+import { Link } from 'react-router-dom';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Navbar = ({ sticky, myClass, navbarItem }) => {
-  const [imageSrc, setImageSrc] = useState("");
+  const [imageSrc, setImageSrc] = useState('');
   const [hasLogged, setHasLogged] = useState(false);
 
   // useEffect(() => {
@@ -29,87 +29,122 @@ const Navbar = ({ sticky, myClass, navbarItem }) => {
       setIsSticky(scrollPosition > 0);
     };
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
   useEffect(() => {
-    const hasLoggedValue = localStorage.getItem("hasLogged");
-    if (hasLoggedValue === "true") {
+    const hasLoggedValue = localStorage.getItem('hasLogged');
+    if (hasLoggedValue === 'true') {
       setHasLogged(true);
-      const userImage = localStorage.getItem("userImage");
+      const userImage = localStorage.getItem('userImage');
       if (userImage) {
         setImageSrc(userImage);
       }
     }
   }, []);
 
-  const handleLoginSuccess = (credentialResponse) => {
+  const handleLoginSuccess = credentialResponse => {
     const credentialResponseDecoded = jwtDecode(credentialResponse.credential);
     setImageSrc(credentialResponseDecoded.picture);
-    localStorage.setItem("hasLogged", "true");
-    localStorage.setItem("userImage", credentialResponseDecoded.picture);
+    localStorage.setItem('hasLogged', 'true');
+    localStorage.setItem('userImage', credentialResponseDecoded.picture);
     setHasLogged(true);
   };
   const handleLinkClick = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   return (
     <GoogleOAuthProvider clientId="165093153283-shjo35g4u2vh5tughu7i1ei04eaq4urc.apps.googleusercontent.com">
       <nav
         id="navbar"
-        className={` ${navbarItem} ${isSticky ? "sticky" : myClass}`}
+        className={`navbar navbar-expand-lg fixed-top${navbarItem} ${
+          isSticky ? 'sticky' : myClass
+        }`}
       >
-        <div className="container navbar-row">
-          <div className="row w-100 ">
-            <div className="nav-left-side col-md-4  my-1">
-              <Link
-                to="/"
-                style={{ color: "black", textDecoration: "none" }}
-                onClick={handleLinkClick}
-              >
-                <div className="d-flex align-items-center">
-                  <div className="nav-logo-container">
-                    <img src={logo} className="logo" alt="logo" />
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse container navbar-row"
+            id="navbarTogglerDemo01"
+          >
+            <div className="row w-100 ">
+              <div className="nav-left-side col-md-4  my-1">
+                <Link
+                  to="/"
+                  style={{ color: 'black', textDecoration: 'none' }}
+                  onClick={handleLinkClick}
+                  className="navbar-brand"
+                >
+                  <div className="d-flex align-items-center">
+                    <div className="nav-logo-container">
+                      <img src={logo} className="logo" alt="logo" />
+                    </div>
+                    <h3 className="navbar-logo px-1 my-0">Travellia</h3>
                   </div>
-                  <h3 className="navbar-logo px-1 my-0">Travellia</h3>
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-4 nav-inner-side d-flex align-items-center my-1 justify-content-center">
-              <ul>
-                <li>Discover</li>
-                <li>Trip</li>
-                <li>Community</li>
-                <li>more</li>
-              </ul>
-            </div>
-            <div className="nav-right-side col-md-4 d-flex align-items-center  my-1">
-              {!hasLogged && <GoogleLogin onSuccess={handleLoginSuccess} />}
-              {hasLogged && (
-                <div className="d-flex justify-content-center align-items-center">
-                  <img
-                    style={{
-                      borderRadius: "50%",
-                      width: "50px",
-                      height: "50px",
-                    }}
-                    src={imageSrc}
-                    alt="dad"
-                  />
-                </div>
-              )}
-              <Link
-                to="/Favourites"
-                onClick={() => {
-                  window.scrollTo({ top: 0, behavior: "smooth" });
-                }}
-              >
-                <FontAwesomeIcon icon={faBookmark} className="shoppingCart" />
-              </Link>
+                </Link>
+              </div>
+              <div className="col-md-4 nav-inner-side d-flex  align-items-center my-1 justify-content-center">
+                <ul className="navbar-nav d-flex flex-row">
+                  <li className="nav-item m-0">
+                    <Link to="/" className="nav-link">
+                      Discover
+                    </Link>
+                  </li>
+                  <li className="nav-item m-0">
+                    <Link to="/" className="nav-link">
+                      Trip
+                    </Link>
+                  </li>
+                  <li className="nav-item m-0">
+                    <Link to="/" className="nav-link">
+                      Community
+                    </Link>
+                  </li>
+                  <li className="nav-item m-0">
+                    <Link to="/" className="nav-link">
+                      More
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="nav-right-side col-md-4 d-flex align-items-center  my-1">
+                {!hasLogged && <GoogleLogin onSuccess={handleLoginSuccess} />}
+                {hasLogged && (
+                  <div className="d-flex justify-content-center align-items-center">
+                    <img
+                      style={{
+                        borderRadius: '50%',
+                        width: '45px',
+                        height: '45px',
+                      }}
+                      src={imageSrc}
+                      alt="dad"
+                    />
+                  </div>
+                )}
+                <Link
+                  to="/Favourites"
+                  onClick={() => {
+                    window.scrollTo({ top: 0, behavior: 'smooth' });
+                  }}
+                >
+                  <FontAwesomeIcon icon={faBookmark} className="shoppingCart" />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
