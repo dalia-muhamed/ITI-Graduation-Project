d129cae8fceb5adff5459896d5744d71c392b4eb
mina karam <minakaramkhalil.28@gmail.com>
2023-11-24 13:23:09 +0200

    mina

--- src/components/slickComponent/SlickComponent.jsx
+++ src/components/slickComponent/SlickComponent.jsx
@@ -49,9 +49,9 @@ const SlickComponent = () => {
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
-          initialSlide: 7,
+          initialSlide: 4,
         },
       },
     ],
