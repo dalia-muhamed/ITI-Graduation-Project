6fda1d5419667093c01d07ad0f1bb63093da99e9
dalia.muhammed <dalia.muhammed1525@gmail.com>
2023-12-04 03:40:19 +0200

    push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import "./App.css";
-import AppRoutes from "./Approuter/AppRoutes";
+import './App.css';
+import AppRoutes from './Approuter/AppRoutes';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Axios } from './axios';
 
 function App() {
-  return (
-    <div className="App">
-      <AppRoutes />
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  const loader = document.getElementById('loader');
+  const [apiData, setApiData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await Axios('Hotels', 'Cairo');
+        console.log(data);
+        setApiData(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loader) {
+    setTimeout(() => {
+      loader.style.display = 'none';
+      setLoading(false);
+    }, 3000);
+  }
+  return <div className="App">{!loading && apiData && <AppRoutes />}</div>;
 }
 
 export default App;
